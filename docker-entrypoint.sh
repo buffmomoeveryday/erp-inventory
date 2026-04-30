b5d5872ee2e81bf28714f0bc633e16e6756e0acc
Siddhartha Khanal <sidkhanal77@gmail.com>
2026-04-30 17:17:55 +0545

    fixed entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 cd /app
 export PATH="/app/.venv/bin:$PATH"
-python manage.py migrate --noinput
-exec gunicorn core.wsgi:application \
+uv run python manage.py migrate --noinput
+exec uv run gunicorn core.wsgi:application \
   --bind "0.0.0.0:${PORT:-8080}" \
   --workers "${GUNICORN_WORKERS:-2}" \
   --threads "${GUNICORN_THREADS:-2}" \
